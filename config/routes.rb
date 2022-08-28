9a0576d6f8a2f3e00c4acece9857bd60c9b4313b
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-28 06:26:52 -0700

    Create a Post model, Posts controller/views, and add corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   devise_for :users
+  resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

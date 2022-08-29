a985ad63b26fe3268b615891bd3623cc8ed1a26d
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-08-29 00:47:05 -0700

    Fill out index action of PostsController and create corresponding view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.build post_params
 
     if @post.save
-      redirect_to action: index
+      redirect_to posts_path
     else
       render :new, status: :unprocessable_entity
     end
